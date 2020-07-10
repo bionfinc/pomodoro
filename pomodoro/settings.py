0dda769cde7c85feebcde990e579c5db3664ebee
David Anderson <davidaanderson@protonmail.com>
2020-07-10 16:20:53 -0700

    Change static files to absolute path

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    normpath(join(BASE_DIR, 'static')),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Configure Django App for Heroku.
 import django_heroku
