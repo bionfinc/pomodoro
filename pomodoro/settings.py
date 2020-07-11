089b427a7c183d855d8ac5b8b13e7f50286d072e
David Anderson <davidaanderson@protonmail.com>
2020-07-11 08:30:21 -0700

    Fix syntax error preventing manage.py from running

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    normpath(join(BASE_DIR, 'static')),
+    os.path.normpath((BASE_DIR + 'static')),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
