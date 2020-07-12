eee4a6b2b217b759a47d55955c03a06498ec29e8
David Anderson <davidaanderson@protonmail.com>
2020-07-12 10:11:15 -0700

    Add UserProfile table

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+
+    def __str__(self):
+        return_value = 'PK: ' + str(self.pk) + ' Score: ' + self.score
+
+        return return_value
+
+    task_length = models.IntegerField()
+    short_rest_length = models.IntegerField()
+    long_rest_length = models.IntegerField()
+    score = models.IntegerField()
