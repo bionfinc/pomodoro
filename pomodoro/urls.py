9b91e83ff48c1a8b3d6c7e09d99fe64c135563c8
David Anderson <davidaanderson@protonmail.com>
2020-07-11 18:14:59 -0700

    Add profile and change-default-times paths

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -19,14 +19,16 @@ from django.contrib.auth import views as auth_views
 
 from pages.views import home_view
 
-from accounts.views import create_account_view
+from accounts.views import create_account_view, profile_view, change_default_times_view
 from timer.views import index_view
-  
+
 urlpatterns = [
     path('', home_view, name='home'),
-    path('index/', index_view, name ='index'),
+    path('index/', index_view, name='index'),
     path('createaccount/', create_account_view),
     path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html")),
     path('logout/', auth_views.LoginView.as_view(template_name="pages/home.html")),
     path('admin/', admin.site.urls),
+    path('profile/', profile_view, name='profile'),
+    path('profile/change-default-times/', change_default_times_view, name='change-default-times')
 ]
