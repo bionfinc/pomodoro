2b3c57956005615ca15b4303893cf986177bae63
Chris Desktop <kuch517@gmail.com>
2020-07-14 12:01:52 -0700

    added sessions and task/rest objects and database backend

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
     'timer.apps.TimerConfig',
+    'usersessions.apps.UserSessionsConfig',
 ]
 
 MIDDLEWARE = [
