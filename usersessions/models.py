69eaa1e0ca8ef436f2ccb8ab00e44a32e5a9d66b
Chris Desktop <kuch517@gmail.com>
2020-07-22 18:37:10 -0700

    Updated the database to reflect discussed changes

--- usersessions/models.py
+++ usersessions/models.py
@@ -12,7 +12,6 @@ class UserSession(models.Model):
 	)
 	session_time_start = models.DateTimeField()
 	session_time_end = models.DateTimeField(null=True)
-	category = models.CharField(max_length=100, null=True) # TODO This might need a seperate table 
 	description = models.TextField(null=True)
 	session_name = models.CharField(max_length=100)
 
@@ -28,27 +27,4 @@ class Task(models.Model):
 	task_name = models.CharField(max_length=100)
 	time_start = models.DateTimeField()
 	time_end = models.DateTimeField(null=True)
-	task_description = models.TextField(null=True)
-	subcategory = models.CharField(max_length=100, null=True)
-
-
-# TODO Model description
-class Rest(models.Model):
-	usersession = models.ForeignKey(
-		UserSession, 
-		on_delete=models.CASCADE,
-		null=True
-	)
-	rest_name = models.CharField(max_length=100)
-	time_start = models.DateTimeField()
-	time_end = models.DateTimeField()
-
-
-# TODO Model description
-class ShortRest(Rest):
-    short_rest_time = models.IntegerField()
-
-
-# TODO Model description
-class LongRest(Rest):
-    long_rest_time = models.IntegerField()
+	category = models.CharField(max_length=100, null=True)
