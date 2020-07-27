7b513e32328acac86ec28d6d69a7042ad6981160
David Anderson <andedavi@oregonstate.edu>
2020-07-26 20:05:30 -0700

    Add upgrade view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-
+from django.http import HttpResponse, HttpRequest
 from accounts.forms import SignUpForm, ChangeDefaultTimesForm
 from .models import UserProfile
 from django.contrib.auth.models import User
@@ -58,3 +58,46 @@ def change_default_times_view(request):
     }
 
     return render(request, 'accounts/change_default_times.html', context)
+
+
+def upgrade(request):
+    plant_number = int(request.GET["plant_id"])
+    upgrade_cost = int(request.GET["plant_cost"])
+
+    if request.user.is_authenticated and request.user.userprofile.score >= upgrade_cost:
+        request.user.userprofile.score = request.user.userprofile.score - upgrade_cost
+
+        if plant_number == 1:
+            plant_stage = request.user.userprofile.plant1_stage
+            if plant_stage < 7:
+                new_stage = plant_stage + 1
+                request.user.userprofile.plant1_stage = new_stage
+            else:
+                new_stage = 1
+                request.user.userprofile.plant1_stage = new_stage
+                request.user.userprofile.award_count = request.user.userprofile.award_count + 1
+
+        elif plant_number == 2:
+            plant_stage = request.user.userprofile.plant2_stage
+            if plant_stage < 7:
+                new_stage = plant_stage + 1
+                request.user.userprofile.plant2_stage = new_stage
+            else:
+                new_stage = 1
+                request.user.userprofile.plant2_stage = new_stage
+                request.user.userprofile.award_count = request.user.userprofile.award_count + 1
+
+        elif plant_number == 3:
+            plant_stage = request.user.userprofile.plant3_stage
+            if plant_stage < 7:
+                new_stage = plant_stage + 1
+                request.user.userprofile.plant3_stage = new_stage
+            else:
+                new_stage = 1
+                request.user.userprofile.plant3_stage = new_stage
+                request.user.userprofile.award_count = request.user.userprofile.award_count + 1
+
+        request.user.userprofile.save()
+        return HttpResponse(request.user.userprofile.score)
+    else:
+        return HttpResponse('0')
