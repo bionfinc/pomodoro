4cfc6ca49c917eefc7ecc0daf29d413b8a4e1631
Chris Desktop <kuch517@gmail.com>
2020-08-01 14:36:40 -0700

    Cleaned up task information

--- usersessions/views.py
+++ usersessions/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db.models import Q, Count, Sum
@@ -15,6 +17,7 @@ def tasks_view(request):
 
     #TODO Make not delete multiple categories
     if request.POST.__contains__('delete'):
+        print(request.POST.get('delete'))
         Task.objects.filter(
                                 task_name__exact=request.POST.get('delete'),
                                 usersession__user__username__exact=user
@@ -22,9 +25,9 @@ def tasks_view(request):
 
     if searchquery != None:
         tasks = Task.objects.filter(
-                                Q(task_name__icontains=searchquery) 
-                                | Q(category__icontains=searchquery),
-                                usersession__user__username__exact=user
+                                    Q(task_name__icontains=searchquery) 
+                                    | Q(category__icontains=searchquery),
+                                    usersession__user__username__exact=user
                                 )
     else:
         tasks = Task.objects.filter(usersession__user__username__exact=user)
@@ -40,6 +43,10 @@ def tasks_view(request):
     for task in tasks:
         # Determine the first start date for the task
         task['first_time_start'] = Task.objects.filter(task_name__exact=task.get('task_name')).earliest('time_start').time_start
+
+        # Format the total task time display
+        task['total_task_time_hr'] = int(task['total_task_time'] / 60)
+        task['total_task_time_min'] = task['total_task_time'] % 60
 
     # Set up the page system for displaying data
     paginator = Paginator(tasks, 10) # Show 10 tasks per page.
@@ -61,11 +68,14 @@ def task_detail_view(request):
 
     #TODO does not get the right individual task, probably better to get a collective
 
-    task = Task.objects.filter(task_name__icontains=task_name).earliest('time_start')
+    gen_task = Task.objects.filter(task_name__icontains=task_name).earliest('time_start')
   
+    subtasks = Task.objects.filter(task_name__icontains=task_name)
+
     context = {
         'user': user,
-        'task': task
+        'gen_task': gen_task,
+        'subtasks' : subtasks
     }
 
     return render(request, 'usersessions/task_detail.html', context)
