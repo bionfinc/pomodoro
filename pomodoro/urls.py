c39c8976cb1c222e9747105b6bb81f3eaf74d8b9
adorgan <42968619+adorgan@users.noreply.github.com>
2020-08-08 07:48:06 -0500

    Revert "Add categories tab with details and delete buttons"

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -18,10 +18,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from usersessions.views import tasks_view, sessions_view, session_detail_view, task_detail_view, categories_view, \
-    create_category_view, manageCategories_view, category_detail_view
+    create_category_view
 from accounts.views import create_account_view, profile_view, change_default_times_view, upgrade, \
     change_profile_information_view
-
 from timer.views import index_view, editTask_view, add_points, deduct_points, is_logged_in, editUserSession_view, \
     save_task_info, editSessionDescription_view, update_task_category, update_task_time_end
 
@@ -50,7 +49,5 @@ urlpatterns = [
     path('upgrade/', upgrade, name='upgrade'),
     path('updateTaskTimeEnd/', update_task_time_end, name='updateTaskTimeEnd'),
     path('categories/', categories_view, name='categories'),
-    path('createCategory/', createCategory_view, name='createCategory'),
-    path('manageCategories/', manageCategories_view, name='manageCategories'),
-    path('categoryDetail/', category_detail_view, name='categoryDetail'),
+    path('createCategory/', create_category_view, name='createCategory'),
 ]
